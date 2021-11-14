7098481e028da41724bd77467962fcced880f894
David Valade <david@valadeservices.com>
2021-11-14 06:52:54 -0500

    Include prompts and basic export

--- intacct_inter_entity_account_mapping_helper/intacct_inter_entity_account_mapping_helper.py
+++ intacct_inter_entity_account_mapping_helper/intacct_inter_entity_account_mapping_helper.py
@@ -21,7 +21,7 @@ print(
 
 def export_file():
     try:
-        with open('iet_config_dv_{}.csv'.format(iet_option), 'w', encoding='UTF8', newline='') as f:
+        with open('iet_config_avt.csv', 'w', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(csv_header)
             writer.writerows(csv_data)
@@ -51,13 +51,13 @@ while True:
 entities = int(entities)
 print('Great! Time to plan for inter-entity accounts!')
 
-
+# fmt: off
 ENT_BASIC       = entities * 2
 ENT_ADV2        = entities ** 2 - entities
 ENT_ADV4        = entities * (entities - 1) * 2
 ENT_BASIC_SIMP  = entities
 ENT_ADV2_SIMP   = int((entities ** 2 - entities) / 2)
-
+# fmt: on
 
 print(
     '''
@@ -74,8 +74,80 @@ For reference, with {0:,d} entities, you could have:
  - Advanced Simpifed Four: {1:,d} accounts
 
 We find that this last option ("Advanced Simplified Four") is the best.
-
 '''.format(
         entities, ENT_BASIC, ENT_ADV2, ENT_ADV4, ENT_BASIC_SIMP, ENT_ADV2_SIMP
     )
 )
+
+print(
+    '''For our entity list, either: 
+ - (1) key entity numbers
+ - (2) import a list of entity IDs
+ - (3) use a dummy list to edit manually later
+''')
+
+while True:
+    print('Pick a option listed (or "quit" to stop):')
+    response = input('> ').upper()
+    if response == 'QUIT':
+        print('Thanks for playing!')
+        sys.exit()
+    if response in ('1', '2', '3'):
+        break
+choice = int(response)
+
+entity_list = []
+if choice == 1:
+    while True:
+        if len(entity_list) == entities: break
+        ent = input('Enter an entity ID (or "QUIT"): ')
+        if ent.upper == 'QUIT': sys.exit()
+        if ent in entity_list:
+            print('Sorry, that entity was entered already.')
+            print('As a reminder, the entities so far are:', ', '.join(entity_list))
+            continue
+        entity_list.append(ent)
+elif choice == 2:
+    '''To import a file'''
+    pass
+elif choice == 3:
+    entity_list = ['ENTITY{}'.format(x + 1) for x in range(entities)]
+
+acct_list = []
+counter = 0
+csv_data = []
+ent_pairs = []
+csv_header = ['ENTITY A', 'ENTITY B', 'ENTITY A IER', 'ENTITY A IEP', 'ENTITY B IER', 'ENTITY B IEP']
+for from_entity in entity_list:
+    for to_entity in entity_list:
+        if from_entity != to_entity:
+            if 'Due from entity {}'.format(to_entity) not in acct_list:
+                counter += 1
+                print(counter, 'Due from entity {}'.format(to_entity))
+                acct_list.append('Due from entity {}'.format(to_entity))
+            if 'Due to entity {}'.format(to_entity) not in acct_list:
+                counter += 1
+                print(counter, 'Due to entity {}'.format(to_entity))
+                acct_list.append('Due to entity {}'.format(to_entity))
+            if 'Due from entity {}'.format(from_entity) not in acct_list:
+                counter += 1
+                print(counter, 'Due from entity {}'.format(from_entity))
+                acct_list.append('Due from entity {}'.format(from_entity))
+            if 'Due to entity {}'.format(from_entity) not in acct_list:
+                counter += 1
+                print(counter, 'Due to entity {}'.format(from_entity))
+                acct_list.append('Due to entity {}'.format(from_entity))              
+            if '{}/{}'.format(from_entity, to_entity) not in ent_pairs and '{}/{}'.format(to_entity, from_entity) not in ent_pairs:
+                ent_pairs.append('{}/{}'.format(from_entity, to_entity))
+                ent_pairs.append('{}/{}'.format(to_entity, from_entity))  
+                csv_data.append(
+                    [
+                        from_entity,
+                        to_entity,
+                        'Due from entity {}'.format(to_entity),
+                        'Due to entity {}'.format(to_entity),
+                        'Due from entity {}'.format(from_entity),
+                        'Due to entity {}'.format(from_entity)
+                    ]
+                )
+export_file()
