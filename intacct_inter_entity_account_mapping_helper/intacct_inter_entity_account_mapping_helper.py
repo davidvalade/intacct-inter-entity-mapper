f6c973b09d2fa18c753598f5e64650195cef9637
David Valade <david@valadeservices.com>
2025-09-02 11:34:28 -0400

    Refactor for better readability

--- intacct_inter_entity_account_mapping_helper/intacct_inter_entity_account_mapping_helper.py
+++ intacct_inter_entity_account_mapping_helper/intacct_inter_entity_account_mapping_helper.py
@@ -1,23 +1,25 @@
 """Intacct Inter-Entity Account Mapper
 Sage Intacct has great functionality to track due-to and due-from balances.
 Thinking through the relationship isn't always obvious, so this can help.
-Tags: Intacct, inter-entity"""
+Tags: Intacct, inter-entity
+"""
 
 #! python3
 
 import sys
 import csv
 
-BAR = chr(9608)  # Character 9608 is '█'
+BAR = chr(9608)  # █
 
 print(
     r'''
-    ___    ____       _    ___      __       ______          __  
-   /   |  / / /_____ | |  / (_)____/ /_____ /_  __/__  _____/ /_ 
-  / /| | / / __/ __ `/ | / / / ___/ __/ __ `// / / _ \/ ___/ __ \
- / ___ |/ / /_/ /_/ /| |/ / (__  ) /_/ /_/ // / /  __/ /__/ / / /
-/_/  |_/_/\__/\__,_/ |___/_/____/\__/\__,_//_/  \___/\___/_/ /_/ 
 
+  __    _ ____    ____    ____    _____   ______
+ \  \  //|    \  |    |  |    \  |     \ |   ___|
+  \  \// |     \ |    |_ |     \ |      \|   ___|
+   \__/  |__|\__\|______||__|\__\|______/|______|
+
+ 
 '''
 )
 
@@ -26,182 +28,140 @@ def export_file():
     """Exports the results to two files. One is a list of accounts, the other
     is a CSV file suitable for import."""
     try:
-        with open('iet_config_avt.csv', 'w', encoding='UTF8', newline='') as f:
+        with open('iet_config_vs.csv', 'w', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(csv_header)
             writer.writerows(csv_data)
     except PermissionError:
-        print("\nHey! Looks like you have our CSV open... that means we skip it!")
-    except:
-        print("\nSomething unexpected happened. No CSV created.")
+        print('\nHey! Looks like you have our CSV open... skipping!')
+    except Exception as e:
+        print(f'\nUnexpected error ({e}). No CSV created.')
     else:
-        print(
-            "\nThere! Made a CSV file as an example as a starting point. Find/Replace a few values and you're good to import."
-        )
-    with open('acct_listing_avt.txt', 'w', encoding='UTF8', newline='') as f:
+        print('\nCreated CSV file as a starting point. Find/Replace a few values and you\'re good to import.')
+
+    with open('acct_listing_vs.txt', 'w', encoding='UTF8', newline='') as f:
         f.write('ACCOUNTS\n')
-        for acct in acct_list:
-            f.write('{}\n'.format(acct))
+        f.writelines(f'{acct}\n' for acct in sorted(acct_set))
 
 
 def import_file():
     """If selected, a user can import a TXT file called entitylist.txt for a list."""
-    print('Create a TXT file called "entitylist.txt" with one entity listed per line.')
-    choice = input('Press enter to load or QUIT to stop: ').upper()
+    print('Create a TXT file called "entitylist.txt" with one entity per line.')
+    choice = input('Press Enter to load or QUIT to stop: ').strip().upper()
     if choice == 'QUIT':
         sys.exit()
     try:
         with open('entitylist.txt', 'r') as f:
-            entity_list = f.readlines()
-            entity_list = [line.rstrip() for line in entity_list]
-            entity_list = list(set(entity_list))        # Remove duplicates
-            entity_list.sort()                          # Sort list
-            entity_list = ' '.join(entity_list).split() # Remove any blank lines
+            entity_list = [line.strip() for line in f if line.strip()]
+            entity_list = sorted(set(entity_list))  # Deduplicate + sort
     except FileNotFoundError:
-        print('Sorry, "entitylist.txt" not found.')
-        print('Create that file and try again.')
+        print('Sorry, "entitylist.txt" not found.\nCreate that file and try again.')
         sys.exit()
     return entity_list
 
 
 def getProgressBar(progress, total, barWidth=40):
-    """Returns a string that represents a progress bar that has barWidth
-    bars and has progressed progress amount out of a total amount."""
+    """Returns a progress bar string with barWidth segments."""
+    progress = max(0, min(progress, total))
+    numBars = int((progress / total) * barWidth)
 
-    progressBar = ''  # The progress bar will be a string value.
-    progressBar += '['  # Create the left end of the progress bar.
-
-    # Make sure that the amount of progress is between 0 and total:
-    if progress > total:
-        progress = total
-    if progress < 0:
-        progress = 0
-
-    # Calculate the number of "bars" to display:
-    numberOfBars = int((progress / total) * barWidth)
-
-    progressBar += BAR * numberOfBars  # Add the progress bar.
-    progressBar += ' ' * (barWidth - numberOfBars)  # Add empty space.
-    progressBar += ']'  # Add the right end of the progress bar.
-
-    # Calculate the percentage complete:
-    percentComplete = round(progress / total * 100, 1)
-    progressBar += ' ' + str(percentComplete) + '%'  # Add percentage.
-
-    # Add the numbers:
-    progressBar += ' ' + str(progress) + '/' + str(total)
-
-    return progressBar  # Return the progress bar string.
+    percent = round(progress / total * 100, 1)
+    return f"[{BAR * numBars}{' ' * (barWidth - numBars)}] {percent}% {progress}/{total}"
 
 
 while True:
-    print('Enter number of entities:')
-    entities = input('> ').upper()
-    if entities.isnumeric() is True:
-        if int(entities) > 0:
-            break
-    else:
-        if len(entities) == 0 or entities == 'QUIT':
-            print('Quitting!')
-            sys.exit()
-entities = int(entities)
+    entities = input('Enter number of entities (or QUIT): ').strip().upper()
+    if entities == 'QUIT' or not entities:
+        print('Quitting!')
+        sys.exit()
+    if entities.isnumeric() and int(entities) > 0:
+        entities = int(entities)
+        break
+
 print('Great! Time to plan for inter-entity accounts!')
 
-# fmt: off
-ENT_BASIC       = entities * 2
-ENT_ADV2        = entities ** 2 - entities
-ENT_ADV4        = entities * (entities - 1) * 2
-ENT_BASIC_SIMP  = entities
-ENT_ADV2_SIMP   = int((entities ** 2 - entities) / 2)
-# fmt: on
+ENT_BASIC = entities * 2
+ENT_ADV2 = entities ** 2 - entities
+ENT_ADV4 = entities * (entities - 1) * 2
+ENT_BASIC_SIMP = entities
+ENT_ADV2_SIMP = (entities ** 2 - entities) // 2
 
-print(
-    '''
-We can set up Basic or Advanced. Many options exist for how to set up
-relationships. 
+print(f'''
+We can set up Basic or Advanced. Many options exist.
 
-For reference, with {0:,d} entities, you could have:
- - Basic: {1:,d} accounts (one payable and receivable acct per entity)
- - Advanced Two: {2:,d} accounts (one payable and receivable per entity pair)
- - Advanced Four: {3:,d} accounts (one payable and receivable per entity relationship)
- - Advanced Basic, single Bal Sheet acct: {4:,d} accounts
- - Advanced Four, single Bal Sheet acct: {2:,d} accounts
- - Advanced Two, single Bal Sheet acct: {5:,d} accounts
- - *** Advanced Simpifed Four: {1:,d} accounts
+For reference, with {entities:,} entities, you could have:
+ - Basic: {ENT_BASIC:,} accounts
+ - Advanced Two: {ENT_ADV2:,} accounts
+ - Advanced Four: {ENT_ADV4:,} accounts
+ - Advanced Basic, single Bal Sheet acct: {ENT_BASIC_SIMP:,} accounts
+ - Advanced Four, single Bal Sheet acct: {ENT_ADV2:,} accounts
+ - Advanced Two, single Bal Sheet acct: {ENT_ADV2_SIMP:,} accounts
+ - *** Advanced Simplified Four: {ENT_BASIC:,} accounts
 
 We find that this last option ("Advanced Simplified Four") is the best.
-'''.format(
-        entities, ENT_BASIC, ENT_ADV2, ENT_ADV4, ENT_BASIC_SIMP, ENT_ADV2_SIMP
-    )
-)
+''')
 
-print(
-    '''For our entity list, either: 
+print('''For our entity list, either: 
  - (1) key entity numbers
  - (2) import a list of entity IDs
  - (3) use a dummy list to edit manually later
-'''
-)
+''')
 
 while True:
-    print('Pick a option listed (or "quit" to stop):')
-    response = input('> ').upper()
+    response = input('Pick an option (1, 2, 3) or QUIT: ').strip().upper()
     if response == 'QUIT':
         print('Thanks for playing!')
         sys.exit()
     if response in ('1', '2', '3'):
+        choice = int(response)
         break
-choice = int(response)
 
-entity_list = []
 if choice == 1:
-    while True:
-        if len(entity_list) == entities:
-            break
-        ent = input('Enter an entity ID (or "quit" to stop): ')
+    entity_list = []
+    while len(entity_list) < entities:
+        ent = input('Enter an entity ID (or QUIT): ').strip()
         if ent.upper() == 'QUIT':
             sys.exit()
         if ent in entity_list:
-            print('Sorry, that entity was entered already.')
-            print('As a reminder, the entities so far are:\n', ', '.join(entity_list))
+            print(f'Sorry, "{ent}" already entered.\nEntities so far: {", ".join(entity_list)}')
             continue
         entity_list.append(ent)
 elif choice == 2:
     entity_list = import_file()
-elif choice == 3:
-    entity_list = ['ENTITY{}'.format(x + 1) for x in range(entities)]
+else:
+    entity_list = [f'ENTITY{x+1}' for x in range(entities)]
 
-acct_list = []
-ent_pairs = []
-csv_header = ['ENTITY A', 'ENTITY B', 'ENTITY A IER', 'ENTITY A IEP', 'ENTITY B IER', 'ENTITY B IEP']
+acct_set = set()
+ent_pair_set = set()
 csv_data = []
+csv_header = ['ENTITY A', 'ENTITY B', 'ENTITY A IER', 'ENTITY A IEP', 'ENTITY B IER', 'ENTITY B IEP']
+
 counter = 0
-for from_entity in entity_list:
-    for to_entity in entity_list:
-        if from_entity != to_entity:
-            if 'Due from entity {}'.format(to_entity) not in acct_list:
-                acct_list.append('Due from entity {}'.format(to_entity))
-            if 'Due to entity {}'.format(to_entity) not in acct_list:
-                acct_list.append('Due to entity {}'.format(to_entity))
-            if 'Due from entity {}'.format(from_entity) not in acct_list:
-                acct_list.append('Due from entity {}'.format(from_entity))
-            if 'Due to entity {}'.format(from_entity) not in acct_list:
-                acct_list.append('Due to entity {}'.format(from_entity))
-            if '{}/{}'.format(from_entity, to_entity) not in ent_pairs:
-                ent_pairs.append('{}/{}'.format(from_entity, to_entity))
-                ent_pairs.append('{}/{}'.format(to_entity, from_entity))
-                csv_data.append(
-                    [
-                        from_entity,
-                        to_entity,
-                        'Due from entity {}'.format(to_entity),
-                        'Due to entity {}'.format(to_entity),
-                        'Due from entity {}'.format(from_entity),
-                        'Due to entity {}'.format(from_entity),
-                    ]
-                )
-                counter += 1
-                barStr = getProgressBar(counter, ENT_ADV2_SIMP)
-                print(barStr, end='', flush=True)
-                print('\b' * len(barStr), end='', flush=True)
+for entity_a in entity_list:
+    for entity_b in entity_list:
+        if entity_a == entity_b:
+            continue
+
+        acct_set.add(f'Due from entity {entity_b}')
+        acct_set.add(f'Due to entity {entity_b}')
+        acct_set.add(f'Due from entity {entity_a}')
+        acct_set.add(f'Due to entity {entity_a}')
+
+        pair = (entity_a, entity_b)
+        rev_pair = (entity_b, entity_a)
+        if pair not in ent_pair_set and rev_pair not in ent_pair_set:
+            ent_pair_set.update([pair, rev_pair])
+            csv_data.append([
+                entity_a,
+                entity_b,
+                f'Due from entity {entity_b}',
+                f'Due to entity {entity_b}',
+                f'Due from entity {entity_a}',
+                f'Due to entity {entity_a}',
+            ])
+            counter += 1
+            barStr = getProgressBar(counter, ENT_ADV2_SIMP)
+            print(f'\r{barStr}', end='', flush=True)
+
+print()
 export_file()
